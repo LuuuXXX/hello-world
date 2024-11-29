93350413d4271fb80827ec1cb04fff93bf177e1a
LuuuXXX <luuux98@163.com>
2024-11-29 15:39:02 +0800

    add docker

--- ci/run.sh
+++ ci/run.sh
@@ -7,8 +7,6 @@ if [ -n "$CI_JOB_NAME" ]; then
     echo "[CI_JOB_NAME=$CI_JOB_NAME]"
 fi
 
-export
-
 if [[ "$CI_JOB_NAME" == *windows* ]]; then
     chmod +x ci/scripts/run-without-docker.sh
     ci/scripts/run-without-docker.sh
