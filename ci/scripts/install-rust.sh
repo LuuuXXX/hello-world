ede4846fb861a0c586ad5432fed315a9035f93f2
LuuuXXX <luuux98@163.com>
2024-11-29 16:59:39 +0800

    add docker

--- ci/scripts/install-rust.sh
+++ ci/scripts/install-rust.sh
@@ -4,26 +4,26 @@
 export RUSTUP_DIST_SERVER="https://rsproxy.cn"
 export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 
-curl --proto '=https' --tlsv1.2 -sSf https://rsproxy.cn/rustup-init.sh | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-config_file="$HOME/.cargo/config.toml"
-mkdir -p "$HOME/.cargo"
-cat <<EOF > "$config_file"
-[source.crates-io]
-replace-with = 'rsproxy-sparse'
+# config_file="$HOME/.cargo/config.toml"
+# mkdir -p "$HOME/.cargo"
+# cat <<EOF > "$config_file"
+# [source.crates-io]
+# replace-with = 'rsproxy-sparse'
 
-[source.rsproxy]
-registry = "https://rsproxy.cn/crates.io-index"
+# [source.rsproxy]
+# registry = "https://rsproxy.cn/crates.io-index"
 
-[source.rsproxy-sparse]
-registry = "sparse+https://rsproxy.cn/index/"
+# [source.rsproxy-sparse]
+# registry = "sparse+https://rsproxy.cn/index/"
 
-[registries.rsproxy]
-index = "https://rsproxy.cn/crates.io-index"
+# [registries.rsproxy]
+# index = "https://rsproxy.cn/crates.io-index"
 
-[net]
-git-fetch-with-cli = true
-EOF
+# [net]
+# git-fetch-with-cli = true
+# EOF
 
 rustc --version
 cargo --version
