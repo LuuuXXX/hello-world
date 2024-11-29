b2007f2b049c9df4ff168a8c0c17862651f947ab
LuuuXXX <luuux98@163.com>
2024-11-29 17:06:23 +0800

    add docker

--- ci/scripts/install-rust.sh
+++ ci/scripts/install-rust.sh
@@ -4,6 +4,8 @@
 export RUSTUP_DIST_SERVER="https://rsproxy.cn"
 export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 
+echo "insecure" > ~/.curlrc
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 . "$HOME/.cargo/env"
