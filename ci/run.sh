bd6a51e511c771793ce2330f8e597bc91094bac5
LuuuXXX <luuux98@163.com>
2024-11-29 15:58:45 +0800

    add docker

--- ci/run.sh
+++ ci/run.sh
@@ -7,7 +7,7 @@ if [ -n "$CI_JOB_NAME" ]; then
     echo "[CI_JOB_NAME=$CI_JOB_NAME]"
 fi
 
-if [[ "$CI_JOB_NAME" == *windows* ]]; then
+if [[ "$CI_JOB_NAME" == *msvc* ]]; then
     chmod +x ci/scripts/run-without-docker.sh
     ci/scripts/run-without-docker.sh
 else
