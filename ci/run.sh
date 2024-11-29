c67efeeb3a40f1beea4a19fbcbbe61e3a47dee71
LuuuXXX <luuux98@163.com>
2024-11-29 14:45:06 +0800

    add docker

--- ci/run.sh
+++ ci/run.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -e
+set -euo pipefail
+IFS=$'\n\t'
 
 if [ -n "$CI_JOB_NAME" ]; then
     echo "[CI_JOB_NAME=$CI_JOB_NAME]"
 fi
 
 if [ -z "${IMAGE+x}" ]; then
-    bash ci/scripts/run-without-docker.sh
+    ci/scripts/run-without-docker.sh
 else
-    bash ci/scripts/run-with-docker.sh "${IMAGE}"
+    ci/scripts/run-with-docker.sh "${IMAGE}"
 fi
