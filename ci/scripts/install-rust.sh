fc2c2eac5b112c72a2d5bfad7097eab42afb613f
LuuuXXX <luuux98@163.com>
2024-11-29 17:17:40 +0800

    add docker

--- ci/scripts/install-rust.sh
+++ ci/scripts/install-rust.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # Install rust via rustup.
-export RUSTUP_DIST_SERVER="https://rsproxy.cn"
-export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
-
 echo "insecure" > ~/.curlrc
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
