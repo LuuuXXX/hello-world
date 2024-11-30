0e389cdd517d56af13549dade18c5dc40ffcdf9d
LuuuXXX <luuux98@163.com>
2024-11-30 10:15:54 +0800

    add docker

--- ci/scripts/run-with-docker.sh
+++ ci/scripts/run-with-docker.sh
@@ -38,4 +38,4 @@ else
 fi
 
 # run ther docker image.
-docker run --workdir /checkout/obj -v "$source_dir:/checkout/obj" --init --rm rim-ci pwd
+docker run --workdir /checkout/obj -v "$source_dir:/checkout/obj" --init --rm rim-ci
