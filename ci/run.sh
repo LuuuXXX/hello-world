57453c817c532844d447a487bb8b3ed233db11a9
LuuuXXX <luuux98@163.com>
2024-11-30 11:06:19 +0800

    add docker

--- ci/run.sh
+++ ci/run.sh
@@ -14,6 +14,3 @@ else
     chmod +x ci/scripts/run-with-docker.sh
     ci/scripts/run-with-docker.sh "${CI_JOB_NAME}"
 fi
-
-chmod +x ci/scripts/package-artifacts.sh
-ci/scripts/package-artifacts.sh
--- ci/scripts/package-artifacts.sh
+++ ci/scripts/package-artifacts.sh
@@ -1,5 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -e
+set -euo pipefail
+IFS=$'\n\t'
 
-echo "Packaging something"
+if [ -n "$CI_JOB_NAME" ]; then
+    echo "[CI_JOB_NAME=$CI_JOB_NAME]"
+fi
+
+if [[ "$CI_JOB_NAME" == *msvc* ]]; then
+    echo "windows"
+else
+    echo "unix"
+fi
